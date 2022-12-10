dca86dec0278bbe1a077bbeee71975c5f401ca79
reonamay <reonamay@gmail.com>
2022-12-10 11:34:53 +0700

    Create HelloFile.java

---
+++ src/com/reona/HelloFile.java
@@ -0,0 +1,10 @@
+package com.reona;
+
+public class HelloFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java Project : This action file I/O");
+	}
+
+}
